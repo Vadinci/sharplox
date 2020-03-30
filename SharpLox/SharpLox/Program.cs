999125a3819bebd991cd5e5740213120fdea49ef
vadinci <info@vadinci.net>
2020-03-30 20:11:13 +0200

    Working through chapter 4

---
+++ SharpLox/SharpLox/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SharpLox
+{
+    class Program
+    {
+        static Boolean hadError = false;
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.Write("Usage: Lox [script]");
+                return;
+            }
+            else if (args.Length == 1)
+            {
+                runFile(args[0]);
+            }
+            else {
+                runPrompt();
+            }
+        }
+
+        
+
+        private static void runFile(string path)
+        {
+            string program = File.ReadAllText(path);
+            run(program);
+
+            if (hadError) return;
+        }
+
+        private static void runPrompt()
+        {
+            while (true) {
+                Console.Write("|> ");
+                run(Console.ReadLine());
+                hadError = false;
+            }
+        }
+
+        private static void run(string program)
+        {
+            Scanner scanner = new Scanner(program);
+            List<Token> tokens = scanner.getTokens();
+
+            for(Token t in tokens) {
+                Console.Write(token);
+            }
+        }
+
+        static void error(int line, string message) {
+            report(line, "", message);
+        }
+
+        private static void report(int line, string where, string message) {
+            Console.Error.Write("[Line "+line+ "] Error" + where + ": " + message);
+        }
+    }
+}
---
+++ SharpLox/SharpLox/Scanner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpLox
+{
+    public class Scanner
+    {
+        private readonly string source;
+        private readonly List<Token> tokens = new List<Token>();
+
+        private int start = 0;
+        private int current = 0;
+        private int line = 1;
+
+        public Scanner(string source)
+        {
+            this.source = source;
+        }
+
+        public List<Token> ScanTokens() {
+            while (!IsAtEnd()) {
+                start = current;
+                ScanToken();
+            }
+
+            tokens.Add(new Token(TokenType.EOF, "", null, line));
+            return tokens;
+        }
+
+        private bool IsAtEnd()
+        {
+            return current >= source.Length;
+        }
+
+        //TODO left at 4.5
+    }
+}
